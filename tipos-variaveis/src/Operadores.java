1d01a3fc1e777bf7f12285ab2e0f00e6f6246325
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-09-27 07:48:06 -0300

    comitando a aula de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,25 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "Linguagem " + "Java";
+       /*  String nomeCompleto = "Linguagem " + "Java";
 
         System.out.println(nomeCompleto);
+
+        int numero =5;
+        numero++;
+        System.out.println(numero); */
+
+        int a = 5;
+        int b = 6;
+
+        if (a==b){
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
     }
-    
 }
