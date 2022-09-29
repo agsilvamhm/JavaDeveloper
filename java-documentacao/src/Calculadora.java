d0bb4d4eab25d4c45625b64bbd01e2c3f312887b
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-09-29 18:46:08 -0300

    Commitando a aula de Documentação

---
+++ java-documentacao/src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * <h1>Calculadora</h1> 
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos orefecidos pelo autor
+ * 
+ * @autor Adalberto Gonçalves
+ * @Version 1.0
+ * @since 29/09/2022
+ * 
+ * 
+ */
+
+public class Calculadora{
+/**
+ * Este método é utilizado para somar dois números inteiros
+ * @param  numeroUm este é o primeiro parâmetro do método
+ * @param numerodois este é o segundo parâmetro do método
+ * @return int o resultado deste método é a soma de dois números
+ */
+public int somar(int numeroUm, int numeroDois){
+    return numeroUm + numeroDois;
+}
+
+}
---
+++ java-documentacao/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // olá eu sou um comentário em uma única linha
+        /*
+         * Olá eu sou um comentário
+         * que posso ser mais detalhado 
+         * quando necessário
+         */
+    }
+
+
+    /**
+     * Estes dois astericos acima 
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação
+     * que incrivel!
+     */
+    public void metodo(){
+
+    }
+}
