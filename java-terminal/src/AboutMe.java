b63bfbaaa3389ab15bd014b76433e8564ef83664
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-09-30 09:32:11 -0300

    Comitando aula final do módulo

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,12 @@
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        String nome = args[0];
+        String sobreNome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobreNome);
+        System.out.println("Tenho " + idade + "anos");
+        System.out.println("Minha altera é " + altura + "cm");
+    }
+}
