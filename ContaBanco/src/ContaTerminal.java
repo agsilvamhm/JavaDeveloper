29e7b802e20f37de1fc40f18c67df2419aa67a97
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-04 14:46:14 -0300

    Comit para a criação do projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+
+    public static void main(String[] args) throws Exception {
+        numero = inputInt()
+
+
+
+    }
+}
