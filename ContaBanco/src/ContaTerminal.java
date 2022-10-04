9f6f776ffa12b118f67fce07987e4534c02dfe1d
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-04 15:48:45 -0300

    Comit final do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    private int numero;
-    private String agencia;
-    private String nomeCliente;
-    private double saldo;
-
-
     public static void main(String[] args) throws Exception {
-        numero = inputInt()
+        Scanner obj = new Scanner(System.in);
 
+        System.out.println("Informe seu nome: ");
+        String nome = obj.nextLine();
 
+        System.out.println("Informe o número da agência: ");
+        String agencia = obj.nextLine();
 
+        System.out.println("Informe o número da sua conta: ");
+        int numero = obj.nextInt();
+
+        System.out.println("Informe o seu saldo: ");
+        double saldo = obj.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +". conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
     }
 }
