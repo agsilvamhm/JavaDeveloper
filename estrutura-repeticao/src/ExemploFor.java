d888abce4678e972d9c36f0ec4b25dc6582de3b2
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-06 08:39:21 -0300

    Comitando o módulo VII

---
+++ estrutura-repeticao/src/ExemploFor.java
@@ -0,0 +1,16 @@
+public class ExemploFor {
+/*
+ * Vamos imaginar que joaozinho precisa contar até 20 carneirinhos para pegar no sono.
+ * 
+ */
+
+    public static void main(String[] args) throws Exception {
+
+        for (int cont = 1; cont <=20 ;cont ++){
+
+            System.out.println("Número de carneirinhos " + cont); 
+        }
+
+        System.out.println("Joãozinho dormiu.");
+    }
+}
